64d45ac62f31b256414769306c44b5975b2aa9b3
Petra Stark <petravstark@gmail.com>
2025-11-04 12:22:04 -0500

    implemented CORS and created an endpoint to hide user's api key in front-end code

--- public/js/index.js
+++ public/js/index.js
@@ -6,21 +6,22 @@ const elements = {
 };
 
 async function getRandomImage() {
-    const client_id = "YOUR-API-KEY";
-    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    const endpoint = "http://localhost:8080/api/v1/getRandomImage";
     try {
         const response = await fetch(endpoint);
         const returnedData = await response.json();
-        const receivedPhotoUrl = returnedData.urls.regular;
+        const receivedPhotoUrl = returnedData.data;
 
         const imgDiv = document.querySelector(".background-img");
-        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+        imgDiv.style.backgroundImage = `url(${receivedPhotoUrl})`;
     } catch (error) {
         console.error(error);
     }
 }
 
+
 getRandomImage();
+
 
 /*
 const quotes = [
